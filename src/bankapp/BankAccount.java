406144d4a3a958d41f22bd070de090cb0fe87f5c
emoseswustl <e.g.moses@wustl.edu>
2024-03-24 16:15:11 -0500

    Finished instance variables and methods

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -3,10 +3,23 @@ package bankapp;
 public class BankAccount {
 	
 	private double balance;
+	private int ID;
+	private boolean isChecking;
+	private User owner;
 	
 	//Constructors - not tested
-	public BankAccount() {
+	public BankAccount(boolean isChecking, User owner) {
 		this.balance = 0;
+		this.ID = (int) (Math.random() * 1000000);
+		this.isChecking = isChecking;
+		this.owner = owner;
+	}
+	
+	public BankAccount(boolean isChecking, User owner, double startBalance) {
+		this.balance = startBalance;
+		this.ID = (int) (Math.random() * 1000000);
+		this.isChecking = isChecking;
+		this.owner = owner;
 	}
 	
 	//public method doing some work - lots of tests
@@ -15,6 +28,24 @@ public class BankAccount {
 			throw new IllegalArgumentException("Amount must be positive");
 		}
 		this.balance += amount;
+	}
+	
+	public double withdraw(double amount) {
+		if(amount > this.balance) {
+			throw new IllegalArgumentException("Amount must be less than or equal to balance");
+		}
+		this.balance -= amount;
+		return amount;
+	}
+	
+	public void transfer(double amount, BankAccount recipient) {
+		double transferAmt = this.withdraw(amount);
+		recipient.deposit(transferAmt);
+	}
+	
+	public double cashOut() {
+		double cashOutAmt = this.balance;
+		return this.withdraw(cashOutAmt);
 	}
 	
 	//getters and setters - not tested
