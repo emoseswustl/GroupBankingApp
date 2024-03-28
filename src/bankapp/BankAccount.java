3eb9bd7eeb3f503a631791fdae2ba8c120b192d6
FortunaKadima <112431774+FortunaKadima@users.noreply.github.com>
2024-03-28 23:42:47 +0000

    fortunakleineliseethan

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -1,24 +1,22 @@
 package bankapp;
 
 public class BankAccount {
-	
-	private double balance;
-	
-	//Constructors - not tested
-	public BankAccount() {
-		this.balance = 0;
-	}
-	
-	//public method doing some work - lots of tests
-	public void deposit(double amount) {
-		if(amount < 0) {
-			throw new IllegalArgumentException("Amount must be positive");
-		}
-		this.balance += amount;
-	}
-	
-	//getters and setters - not tested
-	public double getBalance() {
-		return this.balance;
-	}
+    
+    private double balance;
+    
+    public BankAccount() {
+        this.balance = 0;
+    }
+    
+    public void deposit(double amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        this.balance += amount;
+    }
+    
+    public double getBalance() {
+        return this.balance;
+    }
 }
+
