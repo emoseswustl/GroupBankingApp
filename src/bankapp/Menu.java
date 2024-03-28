fa1dad2bd58b4db5b86a06797dbabf17b83cf51c
kmorrow1 <k.morrow@wustl.edu>
2024-03-28 00:17:15 +0000

    WIP

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -1,18 +1,13 @@
 package bankapp;
-
 import java.util.Scanner;
 import java.util.LinkedList;
-
 import bankapp.User;
-
 import bankapp.BankAccount;
-
 interface InputCaretaker {
 	double getDouble();
 	int getInt();
 	String getString();
 }
-
 class ScannerCaretaker implements InputCaretaker {
 	private Scanner scanner;
 
@@ -35,7 +30,6 @@ class ScannerCaretaker implements InputCaretaker {
 		return scanner.nextInt();
 	}
 }
-
 public class Menu {
 
 	private InputCaretaker caretaker;
@@ -80,9 +74,7 @@ public class Menu {
 			mainMenu.firstIteration = false;
 		}
 	}
-	public void setUser(User user) {
-		this.user = user;
-	}
+	public void setUser(User user) {this.user = user;}
 	//Code that just displays stuff - no tests needed
 	public void displayingOptions() {
 		System.out.println("Choose from the following options: ");
@@ -98,68 +90,77 @@ public class Menu {
 	}
 	public void executeSelectedOption(int option){
 		System.out.println("You have selected option: " + option);
-		if (option == 1) {
-			System.out.println("How much money do you want to deposit?");
-			double amount = getValidUserDeposit();
-			processingUserDeposit(amount);
-		} 
-		else if (option == 2) {
-			System.out.println("How much money do you want to withdraw?");
-			double amount = getValidUserWithdraw();
-			processingUserWithdraw(amount);
-		} 
-		else if (option == 3) {
-			if (accounts.size() < 2) {
-				System.out.println("You need at least two accounts to transfer money");
-				return;
-			}
+		if (option == 1) {optionOne();} 
+		else if (option == 2) {optionTwo();} 
+		else if (option == 3) {optionThree();} 
+		else if (option == 4) {optionFour();} 
+		else if (option == 5) {optionFive();} 
+		else if (option == 6) {optionSix();} 
+		else if (option == 7) {optionSeven();} 
+		else if (option == 8) {optionEight();} 
+	}
+	public void optionOne() {
+		System.out.println("How much money do you want to deposit?");
+		double amount = getValidUserDeposit();
+		processingUserDeposit(amount);
+	}
+	public void optionTwo() {
+		System.out.println("How much money do you want to withdraw?");
+		double amount = getValidUserWithdraw();
+		processingUserWithdraw(amount);
+	}
+	public void optionThree() {
+		if (accounts.size() < 2) {
+			System.out.println("You need at least two accounts to transfer money");
+			return;
+		}
+		
+		System.out.println("Choose from the following account IDs to transfer to: ");
+		accountIDs.clear();
+		
+		for (BankAccount account : accounts) {
+			System.out.println("Account ID: " + account.getID());
+			accountIDs.add(account.getID());
+		}
+		
+		int recipientID = getOption();
+		
+		while (!accountIDs.contains(recipientID)) {
+			System.out.println("Invalid account ID!");
 			System.out.println("Choose from the following account IDs to transfer to: ");
-			accountIDs.clear();
-			for (BankAccount account : accounts) {
-				System.out.println("Account ID: " + account.getID());
-				accountIDs.add(account.getID());
-			}
-			int recipientID = getOption();
-			while (!accountIDs.contains(recipientID)) {
-				System.out.println("Invalid account ID!");
-				System.out.println("Choose from the following account IDs to transfer to: ");
-				for (BankAccount account : accounts) {
-					System.out.println("Account ID: " + account.getID());
-				}
-				recipientID = getOption();
-			}
-		} 
-		else if (option == 4) {
-			System.out.println("Your balance is: " + account.getBalance());
-		} 
-		else if (option == 5) {
-			System.out.println("Account Information: ");
-			for (BankAccount account : accounts) {
-				System.out.println("Account ID: " + account.getID());
-				System.out.println("Account Type: " + (account.isChecking() ? "Checking" : "Savings"));
-				System.out.println("Account Balance: " + account.getBalance());
-			}
-		} 
-		else if (option == 6) {
-			if (accounts.size() < 2) {
-				System.out.println("You need at least two accounts to switch accounts");
-				return;
-			}
-			System.out.println("Choose the account ID to switch to: ");
-			accountIDs.clear();
-			for (BankAccount account : accounts) {
-				System.out.println("Account ID: " + account.getID());
-				accountIDs.add(account.getID());
-			}
-			int switchID = getOption();
-			while (!accountIDs.contains(switchID)) {
-				System.out.println("Invalid account ID!");
-				switchID = getOption();
-			}
-			account = accounts.get(accountIDs.indexOf(switchID));
-		} 
-		else if (option == 7) {
-			System.out.println("Do you want to create a (1) checking account or a (2) savings account? ");
+			for (BankAccount account : accounts) {System.out.println("Account ID: " + account.getID());}
+			recipientID = getOption();
+		}
+	}
+	public void optionFour() {System.out.println("Your balance is: " + account.getBalance());}
+	public void optionFive() {
+		System.out.println("Account Information: ");
+		for (BankAccount account : accounts) {
+			System.out.println("Account ID: " + account.getID());
+			System.out.println("Account Type: " + (account.isChecking() ? "Checking" : "Savings"));
+			System.out.println("Account Balance: " + account.getBalance());
+		}
+	}
+	public void optionSix() {
+		if (accounts.size() < 2) {
+			System.out.println("You need at least two accounts to switch accounts");
+			return;
+		}
+		System.out.println("Choose the account ID to switch to: ");
+		accountIDs.clear();
+		for (BankAccount account : accounts) {
+			System.out.println("Account ID: " + account.getID());
+			accountIDs.add(account.getID());
+		}
+		int switchID = getOption();
+		while (!accountIDs.contains(switchID)) {
+			System.out.println("Invalid account ID!");
+			switchID = getOption();
+		}
+		account = accounts.get(accountIDs.indexOf(switchID));
+	}
+	public void optionSeven() {
+		System.out.println("Do you want to create a (1) checking account or a (2) savings account? ");
 			int accountType = getOption();
 			while (accountType != 1 && accountType != 2) {
 				System.out.println("Invalid account type!");
@@ -173,53 +174,40 @@ public class Menu {
 			else {
 				accounts.add(new BankAccount(false, user, 0.0));
 			}
-		} 
-		else if (option == 8) {
-			if (accounts.size() < 1) {
-				System.out.println("You need at least one account to delete an account");
-				return;
-			}
+	}
+	public void optionEight() {
+		if (accounts.size() < 1) {
+			System.out.println("You need at least one account to delete an account");
+			return;
+		}
+		System.out.println("Choose from the following account IDs to delete an account: ");
+		accountIDs.clear();
+		for (BankAccount account : accounts) {
+			System.out.println("Account ID: " + account.getID());
+			accountIDs.add(account.getID());
+		}
+		int deleteID = getOption();
+		while (!accountIDs.contains(deleteID)) {
+			System.out.println("Invalid account ID!");
 			System.out.println("Choose from the following account IDs to delete an account: ");
-			accountIDs.clear();
 			for (BankAccount account : accounts) {
 				System.out.println("Account ID: " + account.getID());
-				accountIDs.add(account.getID());
 			}
-			int deleteID = getOption();
-			while (!accountIDs.contains(deleteID)) {
-				System.out.println("Invalid account ID!");
-				System.out.println("Choose from the following account IDs to delete an account: ");
-				for (BankAccount account : accounts) {
-					System.out.println("Account ID: " + account.getID());
-				}
-				deleteID = getOption();
-			}
-			accounts.remove(accountIDs.indexOf(deleteID));
-		} 
+			deleteID = getOption();
+		}
+		accounts.remove(accountIDs.indexOf(deleteID));
 	}
-
-	public int getOption() {
-		return caretaker.getInt();
-	}
-
+	public int getOption() {return caretaker.getInt();}
 	public void displayFirstIterationName() {
 		System.out.println("Welcome to the bank!");
 		System.out.println("Enter your name: ");
 	}
-
-	public void displayFirstIterationPassword() {
-		System.out.println("Enter your password: ");
-	}
-
+	public void displayFirstIterationPassword() {System.out.println("Enter your password: ");}
 	public void displayFirstIterationEnd() {
 		System.out.println("Thank you for entering your name and password!");
 		System.out.println("You can now proceed to the main menu. Have Fun!");
 	}
-
-	public String getString() {
-		return caretaker.getString();
-	}
-	
+	public String getString() {return caretaker.getString();}
 	//Code that gets user input
 	//No tests needed...for now (probably discuss in future class)
 	public double getValidUserDeposit() {
@@ -231,7 +219,6 @@ public class Menu {
 		}
 		return amount;
 	}
-
 	public double getValidUserWithdraw() {
 		double amount = caretaker.getDouble();
 		while(amount <= 0 || amount > account.getBalance()) {
@@ -241,28 +228,21 @@ public class Menu {
 		}
 		return amount;
 	}
-
 	public void processingUserWithdraw(double amount) {
 		account.withdraw(amount);
 		System.out.println("Your balance is now: " + account.getBalance());
 	}
-	
 	//Does work - needs tests
 	public void processingUserDeposit(double amount) {
 		account.deposit(amount);
 		System.out.println("Your balance is now: " + account.getBalance());
 	}
-
 	public void addAccount(Boolean isChecking, User user) {
 		BankAccount newAccount = new BankAccount(isChecking, user, 0.0);
 		user.addBankAccount(newAccount, user.getPassword());
 		this.accounts.add(newAccount);
 		this.accountIDs.add(newAccount.getID());
 		this.account = newAccount;
-	}
-
-	
-	public BankAccount getAccount() {
-		return account;
-	}
+	}	
+	public BankAccount getAccount() {return account;}
 }
