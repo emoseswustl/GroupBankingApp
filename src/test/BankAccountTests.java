8a6c4c6def0539a28bb41edabdeedf80c83fbc5c
FortunaKadima <112431774+FortunaKadima@users.noreply.github.com>
2024-04-01 10:43:08 -0500

    Update BankAccountTests.java

--- src/test/BankAccountTests.java
+++ src/test/BankAccountTests.java
@@ -1,39 +1,39 @@
-package test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import bankapp.BankAccount;
 
 class BankAccountTests {
 
-	@Test
-	void testSimpleDeposit() {
-		//1. Setup Objects
-		
-		BankAccount testAccount = new BankAccount();
-		
-		//2. Call the method being tested
-		testAccount.deposit(25);
-		
-		//3. Use assertions to verify results
-		assertEquals(25.0, testAccount.getBalance(), 0.01);	
-	}
-	
-	@Test
-	void testNegativeDeposit() {
-		//1. Setup Objects	
-		BankAccount testAccount = new BankAccount();
-		
-		//2. Call the method being tested
-		try {
-			testAccount.deposit(-25);
-			fail();
-		} catch (IllegalArgumentException e) {
-			//we expect to end up here, -25 is a bad input
-			assertTrue(true);
-		}
-	}
+    private BankAccount testAccount;
 
+    @BeforeEach
+    void setup() {
+        testAccount = new BankAccount();
+    }
+
+    @Test
+    void testInitialization() {
+        assertEquals(0.0, testAccount.getBalance(), 0.01, "New account should have 0 balance");
+    }
+
+    @Test
+    void testSimpleDeposit() {
+        testAccount.deposit(25);
+        assertEquals(25.0, testAccount.getBalance(), 0.01, "Depositing 25 should increase balance to 25");
+    }
+
+    @Test
+    void testNegativeDeposit() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> testAccount.deposit(-25));
+        assertEquals("Amount must be positive", exception.getMessage());
+    }
+
+    @Test
+    void testGetBalance() {
+        testAccount.deposit(200);
+        assertEquals(200.0, testAccount.getBalance(), 0.01, "Balance should be 200 after depositing 200");
+    }
 }
